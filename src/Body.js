aa4c956060cad9426ab67bc924a1fce3df402f7b
Alpesh-Jadav <alpeshjadav11899@gmail.com>
2020-09-02 19:51:42 +0530

    add spinner on hover profile image

--- src/Body.js
+++ src/Body.js
@@ -4,16 +4,21 @@ function Body() {
     return (
         <div className="body">
             <div className="container">
-           
-            <div className="body__profile">
-                
-                <img src="/assets/profile.jpg" alt="Alpesh Jadav"/>
+
+                <div className="body__profile">
+
+
+                    <img src="/assets/profile.jpg" alt="Alpesh Jadav" />
+                    <div className="spinner-container">
+                        <div className="spinner"> </div>
+                        <div className="spinner2"> </div>
+                    </div>
+
+                </div>
+                <div className="name">
+                    <h1><span className="first-name">Alpesh<div className="body__line1"></div></span> <span className="last-name">Jadav <div className="body__line2"></div></span></h1>
+                </div>
             </div>
-            <div className="name">
-                <h1><span className="first-name">Alpesh<div className="body__line1"></div></span> <span className="last-name">Jadav <div className="body__line2"></div></span></h1>
-            </div>
-            </div>
-      
         </div>
     )
 }
