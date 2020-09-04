abf70895df88dab6b008923b7b6ee94e6dae7592
Alpesh-Jadav <alpeshjadav11899@gmail.com>
2020-09-04 08:54:47 +0530

    icon links commit

--- src/Body.js
+++ src/Body.js
@@ -19,6 +19,20 @@ function Body() {
                     <h1><span className="first-name">Alpesh<div className="body__line1"></div></span> <span className="last-name">Jadav <div className="body__line2"></div></span></h1>
                 </div>
             </div>
+            <div className="icons-container">
+                <div className="icons">
+                    <img className="icon" src="/assets/github1.png" alt="github"/>
+                </div>
+                <div className="icons">
+                     <img  className="icon"src="/assets/linkedIn.png" alt="github"/>
+                </div>
+                <div className="icons">
+                      <img className="icon" src="/assets/email.png" alt="github"/>
+                </div>
+                <div className="icons">
+                     <img className="icon" src="/assets/resume.png" alt="github"/>
+                </div>
+            </div>
         </div>
     )
 }
