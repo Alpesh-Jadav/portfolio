fc129f6370282473e80dde112ac0c8634f482d4d
Alpesh-Jadav <alpeshjadav11899@gmail.com>
2020-09-06 19:32:21 +0530

    setting up routes

--- src/App.js
+++ src/App.js
@@ -2,6 +2,11 @@ import React from 'react';
 import './App.css';
 import Header from './Header';
 import Body from './Body';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
 
 function App() {
   return (
